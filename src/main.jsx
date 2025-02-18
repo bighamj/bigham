612d1501f14e04e66999dc1ee4e8287e83f67c6b
bighamj <66083892+bighamj@users.noreply.github.com>
2025-02-18 15:13:25 -0500

    Add sentry back

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-// Sentry.init({
-//   dsn: "https://1b5340247fbd1e8b67c16d63f7b2f91c@o4508536386158592.ingest.us.sentry.io/4508536387993600",
-//   integrations: [
-//     Sentry.browserTracingIntegration(),
-//     Sentry.replayIntegration(),
-//   ],
-//   // Tracing
-//   tracesSampleRate: 1.0, //  Capture 100% of the transactions
-//   // Session Replay
-//   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-//   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-// });
+Sentry.init({
+  dsn: "https://1b5340247fbd1e8b67c16d63f7b2f91c@o4508536386158592.ingest.us.sentry.io/4508536387993600",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
