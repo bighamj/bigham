a1b5a8deb94b302dc25bd2a4f7c1108584dfc57d
bighamj <66083892+bighamj@users.noreply.github.com>
2025-09-03 16:11:42 -0400

    Add statsig

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { ThemeProvider, createTheme } from "@mui/material";
 import Stack from "@mui/material/Stack";
 import React from "react";
 import PageBase from "./components/PageBase";
+import { StatsigProvider, useClientAsyncInit } from "@statsig/react-bindings";
+import { StatsigAutoCapturePlugin } from "@statsig/web-analytics";
+import { StatsigSessionReplayPlugin } from "@statsig/session-replay";
 const About = React.lazy(() => import("./containers/About"));
 const Images = React.lazy(() => import("./containers/Images"));
 const Landing = React.lazy(() => import("./containers/Landing"));
@@ -30,21 +33,34 @@ const App = () => {
     },
   });
 
+  const { client } = useClientAsyncInit(
+    "client-lqUipxwYAu63fWMyAqTznnw94MMUNlOj8u3fvjul1nw",
+    { userID: "a-user" },
+    {
+      plugins: [
+        new StatsigAutoCapturePlugin(),
+        new StatsigSessionReplayPlugin(),
+      ],
+    }
+  );
+
   return (
-    <ThemeProvider theme={theme}>
-      <Stack
-        sx={{
-          margin: 0,
-          padding: 0,
-        }}
-      >
-        <PageBase>
-          <Landing />
-          <About />
-          <Images />
-        </PageBase>
-      </Stack>
-    </ThemeProvider>
+    <StatsigProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <Stack
+          sx={{
+            margin: 0,
+            padding: 0,
+          }}
+        >
+          <PageBase>
+            <Landing />
+            <About />
+            <Images />
+          </PageBase>
+        </Stack>
+      </ThemeProvider>
+    </StatsigProvider>
   );
 };
 
